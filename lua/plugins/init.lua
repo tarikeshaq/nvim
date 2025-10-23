d1488e74b6541e7140a8cb46a75c97088dea6bd7
Tarik Eshaq <tarikeshaq@gmail.com>
2025-10-22 21:36:56 -0700

    adds configs to nvimtree

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -17,7 +17,14 @@ return {
         lazy = false,
         dependencies = { "nvim-tree/nvim-web-devicons" },
         config = function()
-            require("nvim-tree").setup {}
+            require("nvim-tree").setup {
+                update_focused_file = {
+                    enable = true,
+                    update_root = false,
+                },
+                sync_root_with_cwd = true,
+                respect_buf_cwd = true,
+            }
         end,
     },
     {
