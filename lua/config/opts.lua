4b7de7b7645a706ff13ea114c864b5fc4a2083a7
Tarik Eshaq <tarikeshaq@gmail.com>
2025-10-22 21:52:55 -0700

    autoreload buffer commands

--- lua/config/opts.lua
+++ lua/config/opts.lua
@@ -12,3 +12,30 @@ vim.opt.autoindent = true   -- Copies indentation from current line when startin
 vim.opt.writebackup = false
 vim.opt.backup = false
 vim.opt.swapfile = false
+
+-- Auto-reload files changed outside of Neovim
+vim.opt.autoread = true     -- Automatically read files when changed outside of Neovim
+
+-- Set up autocommands to check for external changes
+local autoread_group = vim.api.nvim_create_augroup("autoread", { clear = true })
+
+-- Check for external changes when entering a buffer or gaining focus
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold" }, {
+    group = autoread_group,
+    pattern = "*",
+    callback = function()
+        -- Only check if the buffer is not modified
+        if not vim.bo.modified then
+            vim.cmd("checktime")
+        end
+    end,
+})
+
+-- Notify when a file is changed outside of Neovim
+vim.api.nvim_create_autocmd("FileChangedShellPost", {
+    group = autoread_group,
+    pattern = "*",
+    callback = function()
+        vim.notify("File changed on disk. Buffer reloaded.", vim.log.levels.WARN)
+    end,
+})
